3993304aa598683295223ced1a4562c6f73f75f9
Kenny Campbell <campb194@csusm.edu>
2023-03-20 23:06:47 -0700

    Check for Admin Privileges before continuing

--- tooManyRedirectsChromeError.bat
+++ tooManyRedirectsChromeError.bat
@@ -1,1 +1,8 @@
 @echo off 
+
+:: Check for Admin Privileges 
+whoami /groups | find "S-1-16-12288" && set ELEVATED=true || set ELEVATED=false
+if %ELEVATED%==false (
+echo  "Please start over and right-click on this file,"
+echo  "then select 'Run as administrator' to execute successfully."
+echo.&pause&exit)
