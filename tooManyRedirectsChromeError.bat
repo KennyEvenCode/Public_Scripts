1502d292f067e9002f02125b43fb8a85fbb575dd
Kenny Campbell <campb194@csusm.edu>
2023-03-20 23:07:30 -0700

    give user instructions before continuing

--- tooManyRedirectsChromeError.bat
+++ tooManyRedirectsChromeError.bat
@@ -6,3 +6,16 @@ if %ELEVATED%==false (
 echo  "Please start over and right-click on this file,"
 echo  "then select 'Run as administrator' to execute successfully."
 echo.&pause&exit)
+
+
+echo ""
+echo "At this time, please do the following:"
+echo ""
+:: Clear your Internet Browser Cookies and Cache
+echo "  - Manually clear all cookies and cache from your web browser before continuing."
+echo ""
+:: Scan for Viruses 
+echo "  - Run a Full System Scan and Offline System Scan via Windows Security Console."
+echo ""
+echo "Upon completeing these tasks..."
+pause
